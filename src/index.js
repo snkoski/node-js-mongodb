154ef5b6c7ef8cfd6e56601f68bdf43d087f2e76
snkoski <shawnnkoski@gmail.com>
2018-08-30 15:16:21 -0400

    reinstall node_modules. App, Header, ContestPreview Components. sass file with a little styling

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './components/App';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
+
+
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props)
+//   }
+//   render() {
+//     return (
+//       <h2 className="text-center">
+//         {this.props.headerMessage}
+//       </h2>
+//     )
+//   }
+// }
+//
+//

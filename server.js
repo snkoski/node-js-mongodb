fd6d4a1fa485652abf550def825a620fd4f268e1
snkoski <shawnnkoski@gmail.com>
2018-09-10 11:36:29 -0400

    Setup api route to post new names to db

--- server.js
+++ server.js
@@ -5,7 +5,10 @@ import path from 'path';
 // import fs from 'fs';
 import serverRender from './serverRender';
 import express from 'express';
+import bodyParser from 'body-parser';
+
 const server = express();
+server.use(bodyParser.json());
 
 server.use(sassMiddleware({
   src: path.join(__dirname, 'sass'),
